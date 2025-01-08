9816c07f8d05c0d0aa0addfa7043717c907cb072
keshavkumar <keshavsharma5324@gmail.com>
2025-01-08 17:31:25 +0530

    done dart format

--- lib/screens/training_detail.dart
+++ lib/screens/training_detail.dart
@@ -13,82 +13,91 @@ class _TrainingDetailState extends State<TrainingDetail> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SafeArea(
-      child: Column(
-        children: [
-          Stack(
+      body: SafeArea(
+          child: SingleChildScrollView(
+        child: SizedBox(
+          height: 700,
+          child: Column(
             children: [
               Flexible(
                 flex: 3,
-                child: Image.asset(
-                  widget.trainingItemModel?.imageAsset ?? '',
-                  fit: BoxFit.cover,
+                child: Stack(
+                  children: [
+                    Image.asset(
+                      widget.trainingItemModel?.imageAsset ?? '',
+                      fit: BoxFit.cover,
+                    ),
+                    Positioned(
+                      top: 40,
+                      left: 10,
+                      child: InkWell(
+                        onTap: () {
+                          Navigator.of(context).pop();
+                        },
+                        child: Icon(
+                          Icons.arrow_back_ios,
+                          size: 24,
+                          color: Colors.red,
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      bottom: 10,
+                      left: 10,
+                      child: Text(
+                        widget.trainingItemModel?.name ?? '',
+                        style: TextStyle(
+                            overflow: TextOverflow.ellipsis,
+                            fontSize: 24,
+                            fontWeight: FontWeight.w500,
+                            color: Colors.red),
+                      ),
+                    )
+                  ],
                 ),
               ),
-              Positioned(
-                top: 40,
-                left: 10,
-                child: InkWell(
-                  onTap: () {
-                    Navigator.of(context).pop();
-                  },
-                  child: Icon(
-                    Icons.arrow_back_ios,
-                    size: 24,
-                    color: Colors.red,
+              Flexible(
+                flex: 4,
+                child: Column(children: [
+                  Expanded(
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 20),
+                      child: Text(
+                        widget.trainingItemModel?.description ?? '',
+                        maxLines: 10,
+                        style: TextStyle(overflow: TextOverflow.ellipsis),
+                      ),
+                    ),
                   ),
-                ),
-              ),
-              Positioned(
-                bottom: 10,
-                left: 10,
-                child: Text(
-                  widget.trainingItemModel?.name ?? '',
-                  style: TextStyle(
-                      overflow: TextOverflow.ellipsis,
-                      fontSize: 24,
-                      fontWeight: FontWeight.w500,
-                      color: Colors.red),
-                ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Align(
+                      alignment: Alignment.bottomCenter,
+                      child: InkWell(
+                        onTap: () {
+                          Navigator.of(context).pop();
+                        },
+                        child: Container(
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(2),
+                              color: Colors.red),
+                          padding: EdgeInsets.all(6),
+                          child: Text(
+                            'Go Back',
+                            style: TextStyle(
+                                fontSize: 12,
+                                fontWeight: FontWeight.w500,
+                                color: Colors.white),
+                          ),
+                        ),
+                      )),
+                ]),
               )
             ],
           ),
-          Flexible(
-            flex: 4,
-            child: Column(children: [
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 20),
-                child: Text(
-                  widget.trainingItemModel?.description ?? '',
-                ),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Align(
-                  alignment: Alignment.bottomCenter,
-                  child: InkWell(
-                    onTap: () {
-                      Navigator.of(context).pop();
-                    },
-                    child: Container(
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(2),
-                          color: Colors.red),
-                      padding: EdgeInsets.all(6),
-                      child: Text(
-                        'Go Back',
-                        style: TextStyle(
-                            fontSize: 12,
-                            fontWeight: FontWeight.w500,
-                            color: Colors.white),
-                      ),
-                    ),
-                  )),
-            ]),
-          )
-        ],
-      ),
-    ));
+        ),
+      )),
+    );
   }
 }
